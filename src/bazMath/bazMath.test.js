05d69bef8405ab6d26c7e5e6604f6dc5d161ab27
pyabzz <>
2021-01-03 00:29:16 +0000

    Update test descriptions

--- src/bazMath/bazMath.test.js
+++ src/bazMath/bazMath.test.js
@@ -4,30 +4,30 @@ const Random = require("./../random");
 
 describe('BazMath', function () {
     describe('amplitude', function () {
-        it('should return 0 when vector is an empty array', function () {
+        it('returns 0 when vector is an empty array', function () {
             assert.strictEqual(BazMath.amplitude([]), 0);
         });
 
-        it('should return single value in a vector', function () {
+        it('returns single value in a vector', function () {
             assert.strictEqual(BazMath.amplitude([7]), 7);
         });
 
-        it('should return RMS of a normal vector', function () {
+        it('returns RMS of a normal vector', function () {
             assert.strictEqual(BazMath.amplitude([3, 4]), 5);
         });
     });
 
     describe('sigmoid', function () {
-        it('should return 0 at origin', function () {
+        it('returns 0 at origin', function () {
             assert.strictEqual(BazMath.sigmoid(0), 0);
         });
 
-        it('should be symmetric', function () {
+        it('is symmetric', function () {
             const x = Random.getInRange(0.01, 1000);
             assert.strictEqual(BazMath.sigmoid(x), - BazMath.sigmoid(-x));
         });
 
-        it('should return correct value', function () {
+        it('returns correct value', function () {
             assert.strictEqual(BazMath.sigmoid(1), 1 / 2);
         });
     });
--- src/vars/vars.test.js
+++ src/vars/vars.test.js
@@ -3,105 +3,105 @@ const Vars = require("./vars");
 
 describe('Vars', function () {
     describe('isDefined', function () {
-        it('should return false from missing parameter', function () {
+        it('returns false from missing parameter', function () {
             assert.strictEqual(Vars.isDefined(), false);
         });
-        it('should return false from undefined value', function () {
+        it('returns false from undefined value', function () {
             assert.strictEqual(Vars.isDefined(undefined), false);
         });
-        it('should return true from any ordinary value', function () {
+        it('returns true from any ordinary value', function () {
             assert.strictEqual(Vars.isDefined(1), true);
         });
-        it('should return true from 0', function () {
+        it('returns true from 0', function () {
             assert.strictEqual(Vars.isDefined(0), true);
         });
-        it('should return true from null', function () {
+        it('returns true from null', function () {
             assert.strictEqual(Vars.isDefined(null), true);
         });
-        it('should return true from false', function () {
+        it('returns true from false', function () {
             assert.strictEqual(Vars.isDefined(false), true);
         });
-        it('should return true from true', function () {
+        it('returns true from true', function () {
             assert.strictEqual(Vars.isDefined(true), true);
         });
-        it('should return true from empty string', function () {
+        it('returns true from empty string', function () {
             assert.strictEqual(Vars.isDefined(""), true);
         });
     });
 
     describe('isUndefined', function () {
-        it('should return true from missing parameter', function () {
+        it('returns true from missing parameter', function () {
             assert.strictEqual(Vars.isUndefined(), true);
         });
-        it('should return true from undefined value', function () {
+        it('returns true from undefined value', function () {
             assert.strictEqual(Vars.isUndefined(undefined), true);
         });
-        it('should return false from any ordinary value', function () {
+        it('returns false from any ordinary value', function () {
             assert.strictEqual(Vars.isUndefined(1), false);
         });
-        it('should return false from 0', function () {
+        it('returns false from 0', function () {
             assert.strictEqual(Vars.isUndefined(0), false);
         });
-        it('should return false from null', function () {
+        it('returns false from null', function () {
             assert.strictEqual(Vars.isUndefined(null), false);
         });
-        it('should return false from false', function () {
+        it('returns false from false', function () {
             assert.strictEqual(Vars.isUndefined(false), false);
         });
-        it('should return false from true', function () {
+        it('returns false from true', function () {
             assert.strictEqual(Vars.isUndefined(true), false);
         });
-        it('should return false from empty string', function () {
+        it('returns false from empty string', function () {
             assert.strictEqual(Vars.isUndefined(""), false);
         });
     });
 
     describe('isFunction', function () {
-        it('should return false from missing parameter', function () {
+        it('returns false from missing parameter', function () {
             assert.strictEqual(Vars.isFunction(), false);
         });
-        it('should return false from undefined value', function () {
+        it('returns false from undefined value', function () {
             assert.strictEqual(Vars.isFunction(undefined), false);
         });
-        it('should return false from number', function () {
+        it('returns false from number', function () {
             assert.strictEqual(Vars.isFunction(0), false);
         });
-        it('should return false from null', function () {
+        it('returns false from null', function () {
             assert.strictEqual(Vars.isFunction(null), false);
         });
-        it('should return false from boolean', function () {
+        it('returns false from boolean', function () {
             assert.strictEqual(Vars.isFunction(true), false);
         });
-        it('should return false from empty string', function () {
+        it('returns false from empty string', function () {
             assert.strictEqual(Vars.isFunction(""), false);
         });
-        it('should return true from inline function', function () {
+        it('returns true from inline function', function () {
             assert.strictEqual(Vars.isFunction(function () { }), true);
         });
-        it('should return true from arrow function', function () {
+        it('returns true from arrow function', function () {
             assert.strictEqual(Vars.isFunction(() => { }), true);
         });
     });
 
     describe('ifFunctionRun', function () {
-        it('should return value from inline func', function () {
+        it('returns value from inline func', function () {
             assert.strictEqual(Vars.ifFunctionRun(function () { return 2 }), 2);
         });
-        it('should return value from arrow func', function () {
+        it('returns value from arrow func', function () {
             assert.strictEqual(Vars.ifFunctionRun(() => 3), 3);
         });
-        it('should invoke non-return func', function () {
+        it('invokes non-return func', function () {
             const mockObj = {};
             Vars.ifFunctionRun(() => { mockObj.foo = "bar" });
             assert.strictEqual(mockObj.foo, "bar");
         });
-        it('should return undefined from non-return func', function () {
+        it('returns undefined from non-return func', function () {
             assert.strictEqual(Vars.ifFunctionRun(function () { }), undefined);
         });
-        it('should return undefined from null', function () {
+        it('returns undefined from null', function () {
             assert.strictEqual(Vars.ifFunctionRun(null), undefined);
         });
-        it('should return undefined from number', function () {
+        it('returns undefined from number', function () {
             assert.strictEqual(Vars.ifFunctionRun(2), undefined);
         });
     });
