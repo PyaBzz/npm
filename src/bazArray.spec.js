db6275588ac4ad4e98b2d80ef82992935719aa0f
pyabzz <>
2021-01-07 23:51:34 +0000

    Add tests

--- src/bazArray.spec.js
+++ src/bazArray.spec.js
@@ -5,10 +5,9 @@ const BazArray = require("./bazArray");
 
 describe('BazArray', function () {
     describe('takeFirstOut', function () {
-        it('throws error if not enough elements', function () {
+        it('complains if not enough elements', function () {
             const arr = [];
-            const res = BazArray.takeFirstOut(arr, 1);
-            assert.strictEqual(res.length, 0);
+            assert.throws(() => BazArray.takeFirstOut(arr, 1), { message: "Element count cannot exceed array length" });
         });
         it('gets empty array if count is 0', function () {
             const arr = [1, 2, 3];
